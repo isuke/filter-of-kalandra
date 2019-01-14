729a9479df88574fb5ba5218c26c795ee9a110e0
isuke <isuke770@gmail.com>
2019-01-14 12:41:39 +0900

    feat(Color): improve color notation

--- src/views/Color.vue
+++ src/views/Color.vue
@@ -14,9 +14,9 @@
       tr(v-for="(color, i) in $store.state.colors")
         th: input(type="text", v-model="color.name")
         th: input(type="color", v-model="color.hex")
-        th {{ $store.getters.colorObject(i).rgb().color }}
+        th {{ getRGBStr(i) }}
         th {{ color.hex }}
-        th {{ $store.getters.colorObject(i).hsl().color.map(c => Math.round(c)) }}
+        th {{ getHSLStr(i) }}
         th {{ round($store.getters.colorObject(i).luminosity()) }}
         th {{ $store.getters.colorObject(i).isLight() }}
 
@@ -26,6 +26,12 @@
 <script lang="coffee">
 export default
   methods:
+    getRGBStr: (index) ->
+      color = @$store.getters.colorObject(index).rgb().color
+      "#{color[0]} #{color[1]} #{color[2]}"
+    getHSLStr: (index) ->
+      color = @$store.getters.colorObject(index).hsl().color.map((c) => Math.round(c))
+      "#{color[0]}° #{color[1]}% #{color[2]}%"
     # TODO: move to utils
     round: (val, precision = 2) ->
       digit = Math.pow(10, precision)
