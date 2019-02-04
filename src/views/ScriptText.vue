2407b3c8911274ecb41d0cdfb6af9a62033592fe
isuke <isuke770@gmail.com>
2019-02-05 00:55:27 +0900

    perf(ScriptText): change from codemirror to textarea

--- src/views/ScriptText.vue
+++ src/views/ScriptText.vue
@@ -1,10 +1,11 @@
 <template lang="pug">
 .script-text
   header.header
-    ul
-      li(v-for="scriptName in $store.getters.scriptNames")
-        a(@click.prevent="changeCurrentScript(scriptName)", href="") {{ scriptName }}
-  filter-editor.text(v-model="$store.getters.simpleScriptTexts[currentScriptName]", mode="original-poe-filter", :config="{ readOnly: true, lineWrapping: true }")
+    ul.list
+      li.item(v-for="scriptName in $store.getters.scriptNames")
+        a.scriptname(@click.prevent="changeCurrentScript(scriptName)", href="") {{ scriptName }}
+  //- filter-editor.text(v-model="$store.getters.simpleScriptTexts[currentScriptName]", mode="original-poe-filter", :config="{ readOnly: true, lineWrapping: true }")
+  textarea.textarea(v-model="$store.getters.simpleScriptTexts[currentScriptName]", readOnly)
 </template>
 
 <script lang="coffee">
@@ -23,8 +24,34 @@ export default
 
 <style lang="scss" scoped>
 .script-text {
-  > .text {
-    max-width: calc(100vw - #{$aside-size-width});
+  background-color: $global-bg-color-day;
+  color: $global-ft-color-day;
+
+  > .header {
+    > .list {
+      display: inline-flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      height: $sub-header-height;
+      padding-left: var(--space-size-s);
+
+      > .item {
+        flex: 1;
+      }
+    }
+  }
+
+  // > .text {
+  //   width: calc(100vw - #{$aside-size-width});
+  // }
+  > .textarea {
+    font-family: $mono-font-family;
+    padding: 0.5em;
+    width: calc(100vw - #{$aside-size-width});
+    height: calc(100% - #{$sub-header-height});
+    // height: calc(100vh - #{$global-header-height} - #{$sub-header-height})
   }
 }
 </style>
