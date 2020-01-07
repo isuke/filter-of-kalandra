f13743c4969fc65bd96b8d215a6f75930b247b68
isuke <isuke770@gmail.com>
2020-01-07 14:12:51 +0900

    up: upgrade dependencies

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
   css: {
     loaderOptions: {
       sass: {
-        data: `
+        prependData: `
           @import "@/styles/_variables.scss";
           @import "@/styles/_mixins.scss";
         `
