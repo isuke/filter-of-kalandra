2e8e17b8f050f7a1a97f3b7b59ea663aa7ea1540
isuke <isuke770@gmail.com>
2020-01-22 16:50:24 +0900

    ui: add brave logo to GlobalHint

--- src/components/TheGlobalHint.vue
+++ src/components/TheGlobalHint.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
 .the-global-hint
-  p.message The site support only latest chrome and firefox on desktop.
+  p.message
+    | The site support only latest Chrome, Firefox and
+    a.link(href="https://brave.com/fil720" target="_blank" rel="noopener noreferrer"): img.bravelogo(src="@/assets/images/brave-logotype-light.svg")
+    | on desktop.
 </template>
 
 <script lang="coffee">
@@ -31,6 +34,21 @@ export default {}
     white-space: nowrap;
 
     &::-webkit-scrollbar { display: none; }
+
+    > .link {
+      display: inline-flex;
+      flex-direction: row;
+      align-items: center;
+      height: 100%;
+
+      &::after { display: none; }
+
+      > .bravelogo {
+        height: 75%;
+        margin-left: 0.5em;
+        margin-right: 0.5em;
+      }
+    }
   }
 }
 </style>
